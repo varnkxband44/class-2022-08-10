902f5a96ca050a56948791ed29611bd43c917a02
varnkxband44 <varunskhedkar@gmail.com>
2022-08-10 20:58:19 -0400

    clean data + index

--- clean_data.R
+++ clean_data.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-read_csv("social_capital_county.csv") |>  
+ec <- read_csv("social_capital_county.csv") |>  
   select(county, county_name, pop2018, ec_county) |>  
   drop_na() |> 
   mutate(state = str_remove(county_name, ".*, "))
